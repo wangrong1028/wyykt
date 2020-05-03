a193111c448287887c1b32c5b87dc918b8a535f1
wangrong1028 <wr18926279165@163.com>
2020-05-04 00:43:35 +0800

    首页

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,73 @@
 <template>
   <div class="page-home">
-    <h1>首页</h1>
+    <header class="j-mw-header">
+      <div id="j-mw-header" class="mw-header">
+        <a class="mw-header_logo ga-click" href="/" data-category="全局导航" data-action="返回首页"></a>
+        <div
+          id="j-mw-header_app"
+          class="mw-header_app j-mw-header_app"
+          data-category="wap_全局导航"
+          data-action="app查看"
+          style="display: block;"
+        >App查看</div>
+        <div class="mw-header_search" data-category="wap_全局导航" data-action="搜索图标点击">
+          <a href="../Search/index.vue" class="mw-header_search_a ux-icon-search">搜索</a>
+        </div>
+      </div>
+    </header>
+    <div class="mw-firstCateIconSet">
+      <div class="icon-box">
+        <!--Regular list-->
+        <div class="icon-cell">
+          <a href="https://m.study.163.com/smartSpec/intro.htm" data-log-act="click" data-log-data="{&quot;itemId&quot;:null,&quot;itemName&quot;:&quot;微专业&quot;,&quot;itemUrl&quot;:&quot;https://m.study.163.com/smartSpec/intro.htm&quot;,&quot;sceneLog&quot;:&quot;首页图标&quot;}"><div class="icon-round"><img src="//edu-image.nosdn.127.net/fbbf1cfc-45f2-4a42-bcbb-df5cad63d3c2.png?imageView&amp;quality=100&amp;thumbnail=103y103"></div></a>
+          <div class="icon-txt">微专业</div>
+        </div>
+        <div class="icon-cell">
+          <a href="https://m.study.163.com/category/type1/480000003124025" data-log-act="click" data-log-data="{&quot;itemId&quot;:null,&quot;itemName&quot;:&quot;职场提升&quot;,&quot;itemUrl&quot;:&quot;https://m.study.163.com/category/type1/480000003124025&quot;,&quot;sceneLog&quot;:&quot;首页图标&quot;}"><div class="icon-round"><img src="//edu-image.nosdn.127.net/23444883-fc1a-4972-8a22-591e5b790ee0.png?imageView&amp;quality=100&amp;thumbnail=103y103"></div></a>
+          <div class="icon-txt">职场提升</div>
+        </div>
+        <div class="icon-cell">
+          <a href="https://m.study.163.com/category/type1/480000003121004" data-log-act="click" data-log-data="{&quot;itemId&quot;:null,&quot;itemName&quot;:&quot;编程开发&quot;,&quot;itemUrl&quot;:&quot;https://m.study.163.com/category/type1/480000003121004&quot;,&quot;sceneLog&quot;:&quot;首页图标&quot;}"><div class="icon-round"><img src="//edu-image.nosdn.127.net/b216231d-4b93-4d5c-8e7c-e22389f194b8.png?imageView&amp;quality=100&amp;thumbnail=103y103"></div></a>
+          <div class="icon-txt">编程开发</div>
+        </div>
+        <div class="icon-cell icon-right">
+          <a href="https://m.study.163.com/category/type1/480000003131012" data-log-act="click" data-log-data="{&quot;itemId&quot;:null,&quot;itemName&quot;:&quot;人工智能&quot;,&quot;itemUrl&quot;:&quot;https://m.study.163.com/category/type1/480000003131012&quot;,&quot;sceneLog&quot;:&quot;首页图标&quot;}"><div class="icon-round"><img src="//edu-image.nosdn.127.net/55329cd44f7142c7b2eaaca1a68e2430.png?imageView&amp;quality=100&amp;thumbnail=103y103"></div></a>
+          <div class="icon-txt">人工智能</div>
+        </div>
+        <div class="icon-cell">
+          <a href="https://m.study.163.com/category/type1/480000003128005" data-log-act="click" data-log-data="{&quot;itemId&quot;:null,&quot;itemName&quot;:&quot;产品与运营&quot;,&quot;itemUrl&quot;:&quot;https://m.study.163.com/category/type1/480000003128005&quot;,&quot;sceneLog&quot;:&quot;首页图标&quot;}"><div class="icon-round"><img src="//edu-image.nosdn.127.net/8f2db166988542c2b720f418e6330500.png?imageView&amp;quality=100&amp;thumbnail=103y103"></div></a>
+          <div class="icon-txt">产品与运营</div>
+        </div>
+        <div class="icon-cell">
+          <a href="https://m.study.163.com/category/type1/480000003121055" data-log-act="click" data-log-data="{&quot;itemId&quot;:null,&quot;itemName&quot;:&quot;设计创意&quot;,&quot;itemUrl&quot;:&quot;https://m.study.163.com/category/type1/480000003121055&quot;,&quot;sceneLog&quot;:&quot;首页图标&quot;}"><div class="icon-round"><img src="//edu-image.nosdn.127.net/dd3fdff7aeea4b37af8d623be3695331.png?imageView&amp;quality=100&amp;thumbnail=103y103"></div></a>
+          <div class="icon-txt">设计创意</div>
+        </div>
+        <div class="icon-cell">
+          <a href="https://m.study.163.com/category/type1/480000003120022" data-log-act="click" data-log-data="{&quot;itemId&quot;:null,&quot;itemName&quot;:&quot;电子商务&quot;,&quot;itemUrl&quot;:&quot;https://m.study.163.com/category/type1/480000003120022&quot;,&quot;sceneLog&quot;:&quot;首页图标&quot;}"><div class="icon-round"><img src="//edu-image.nosdn.127.net/c0c090e1872748ff8cec40ed03b3e476.png?imageView&amp;quality=100&amp;thumbnail=103y103"></div></a>
+          <div class="icon-txt">电子商务</div>
+        </div>
+        <div class="icon-cell icon-right">
+          <a href="https://m.study.163.com/category/type-1/480000003126038" data-log-act="click" data-log-data="{&quot;itemId&quot;:null,&quot;itemName&quot;:&quot;语言学习&quot;,&quot;itemUrl&quot;:&quot;https://m.study.163.com/category/type-1/480000003126038&quot;,&quot;sceneLog&quot;:&quot;首页图标&quot;}"><div class="icon-round"><img src="//edu-image.nosdn.127.net/fd322af24d8947a8961b99c42b8261e8.png?imageView&amp;quality=100&amp;thumbnail=103y103"></div></a>
+          <div class="icon-txt">语言学习</div>
+        </div>
+      </div>
+    </div>
+    <div class="mw-divide"></div>
+    <div class="mw-inner-box">
+      <div class="mw-custom-module">
+        <div class="custom-top f-cb">
+          <div class="custom-name">畅销好课</div>
+        </div>
+        <div class="f-cb">
+          <div class="custom-card custom-card-small f-fl custom-card-withPrice">
+            <a href=""></a>
+          </div>
+          <div class="custom-card custom-card-small f-fr custom-card-withPrice">
+            <a href=""></a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,3 +76,109 @@ export default {
   name: 'Home'
 }
 </script>
+
+<style lang="scss" scoped>
+.page-home {
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  height: 100%;
+
+  .mw-header {
+    height: 42px;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 12px 8px 0 12px;
+    background-color: #fff;
+  }
+
+  .mw-header_logo {
+    display: block;
+    float: left;
+    height: 20px;
+    width: 98px;
+    margin-right: 7px;
+    background: #fff
+      url(//nos.netease.com/edu-image/a9333338-6b0e-4e56-a34a-77b2a2268b4a.png?imageView&quality=100)
+      center center no-repeat;
+    background-size: contain;
+  }
+
+  .mw-header_app {
+    height: 18px;
+    width: 53px;
+    float: left;
+    box-sizing: border-box;
+    color: #2cc17b;
+    line-height: 18px;
+    text-align: center;
+    border-radius: 18px;
+    font-size: 12px;
+    border: 1px solid #2cc17b;
+  }
+
+  .mw-header_search {
+    float: right;
+  }
+
+  .mw-firstCateIconSet {
+    margin-top: 148px;
+    margin-bottom: 23px;
+  }
+
+  .mw-firstCateIconSet .icon-box {
+    display: table;
+    margin: 0 auto;
+    width: 92%;
+    font-size: 0;
+  }
+
+  .mw-firstCateIconSet .icon-box .icon-cell {
+    display: inline-block;
+    margin-top: 17px;
+    width: 25%;
+  }
+
+  .mw-firstCateIconSet .icon-box .icon-cell .icon-round {
+    margin: 0 auto;
+    width: 32px;
+    height: 32px;
+    border-radius: 0.50667rem;
+    line-height: 32px;
+    color: #fff;
+    text-align: center;
+  }
+
+  .mw-firstCateIconSet .icon-box .icon-cell .icon-round img {
+    width: 100%;
+    height: 100%;
+  }
+
+  .mw-firstCateIconSet .icon-box .icon-cell .icon-txt {
+    margin-top: 7px;
+    text-align: center;
+    font-size: 14px;
+  }
+
+  .mw-divide {
+    width: 100%;
+    height: 12px;
+    background-color: #f2f4f7;
+  }
+
+  .mw-inner-box {
+    display: table;
+    margin: 0 auto;
+    width: 92%;
+
+    .mw-custom-module {
+      padding-top: 17px;
+
+      .custom-top {
+        margin-bottom: 12px;
+      }
+    }
+  }
+
+}
+</style>
